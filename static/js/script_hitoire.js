2de13731625320ca32b16688a66b1785ee2fe291
Mickaelcrea <mickael.crea@gmail.com>
2023-04-20 22:09:35 +0200

    avancement histoire

--- static/js/script_hitoire.js
+++ static/js/script_hitoire.js
@@ -22,30 +22,63 @@ setTimeout(function() {
   },);
 
 
-// const clickableDiv = document.getElementById("click1");
-// clickableDiv.addEventListener("click1", function() {
-//  // Crée un nouvel élément DIV avec une classe pour l'animation
-//  const overlay = document.createElement('div');
-//  overlay.classList.add('overlay');
+  
+  if (window.location.href.endsWith("z-histoire_act_2.html")) {
+   const images = [
+      "static/image/act2A.png",
+      "static/image/act2.png",
+   ];
+ 
+   const image = document.getElementById("acttransi");
+   let currentIndex = 0;
+ 
+   function transitionImage() {
+      currentIndex++;
+      if (currentIndex >= images.length) {
+         return;
+      }
+      image.style.opacity = 0;
+      setTimeout(() => {
+         image.src = images[currentIndex];
+         image.style.opacity = 1;
+         image.addEventListener("transitionend", transitionImageOnce);
+      }, );
+   }
+ 
+   function transitionImageOnce() {
+      image.removeEventListener("transitionend", transitionImageOnce);
+   }
+ 
+   setTimeout(transitionImage, 1000);
+ }
 
-//  // Ajoute la couleur d'arrière-plan noire, la propriété z-index élevée et la position relative à l'élément body
-//  document.body.style.overflow = "hidden"
-//  overlay.style.background = "transparent";
-//  overlay.style.transition = "background-color 0.2s ease";
-//  overlay.style.position = "absolute";
-//  overlay.style.zIndex = "1000000";
-//  overlay.style.display = "block";
-//  overlay.style.width = "400vh";
-//  overlay.style.height = "400vh";
-//  overlay.style.top = "0";
-//  document.body.appendChild(overlay);
+const clickableDiv = document.getElementById("click1");
+clickableDiv.addEventListener("click1", function() {
+
+   fetch("z-histoire_act_2.html");
+ // Crée un nouvel élément DIV avec une classe pour l'animation
+ const overlay = document.createElement('div');
+ overlay.classList.add('overlay');
+
+ // Ajoute la couleur d'arrière-plan noire, la propriété z-index élevée et la position relative à l'élément body
+ document.body.style.overflow = "hidden"
+ overlay.style.background = "transparent";
+ overlay.style.transition = "background-color 0.2s ease";
+ overlay.style.position = "absolute";
+ overlay.style.zIndex = "1000000";
+ overlay.style.display = "block";
+ overlay.style.width = "400vh";
+ overlay.style.height = "400vh";
+ overlay.style.top = "0";
+ document.body.appendChild(overlay);
 
  
-//  setTimeout(function() {
-//    overlay.style.backgroundColor = "black";
-//  }, 200);
-//  // Redirige l'utilisateur vers la nouvelle page après la transition
-//  setTimeout(function () {
-//    window.location.href = "z-histoire_act_2.html";
-//  }, 500);
-// });
+ setTimeout(function() {
+   overlay.style.backgroundColor = "black";
+ }, 800);
+ // Redirige l'utilisateur vers la nouvelle page après la transition
+ setTimeout(function () {
+   window.location.href = "z-histoire_act_2.html";
+ }, 1000);
+});
+
